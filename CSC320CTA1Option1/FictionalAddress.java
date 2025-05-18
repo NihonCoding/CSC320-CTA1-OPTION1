ba6ea1aa07c5ffc962d37185a06c55e2b4eddd62
NihonCoding <163963430+NihonCoding@users.noreply.github.com>
2025-05-17 23:15:19 -0500

    Add files via upload

---
+++ CSC320CTA1Option1/FictionalAddress.java
@@ -0,0 +1,17 @@
+
+public class FictionalAddress {
+
+	public static void main(String[] args) {
+		String firstName = "Titus";
+		String lastName = "Valus";
+		String address = "1234 Empire Dr";
+		String city = "Ultramarine";
+		String zipCode = "12012";
+		
+		System.out.println(firstName + "\n" + lastName + "\n" + address + "\n"+ city + "\n" + zipCode);
+		
+		
+
+	}
+
+}
